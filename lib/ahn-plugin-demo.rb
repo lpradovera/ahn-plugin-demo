07ab5a7b78a4729806ea48de36dc535da8cf3988
juandebravo <juandebravo@gmail.com>
2011-10-19 20:30:02 +0200

    MINOR some logic

--- lib/ahn-plugin-demo.rb
+++ lib/ahn-plugin-demo.rb
@@ -1,3 +1,31 @@
 #Dummy plugin
+
 class AhnPluginDemo < Adhearsion::Plugin
+
+  class << self
+    def init
+      define_method :adh_plugin_demo do
+        create_say_text(self).start
+      end
+    end
+  
+    def create_say_text(value)
+      SayText.new(value)
+    end
+  end
+  
+
+  class SayText
+
+    def initialize(call)
+      @call = call
+    end
+
+    def start
+      10.times do
+        @call.play %W['Hello world']
+      end
+    end
+  end
+  
 end
